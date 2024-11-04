99ec7aa76c6cfcb1b04a2ea051de6b1462e850ea
jhuan280 <92900160+jhuan280@users.noreply.github.com>
2024-11-04 13:24:50 -0800

    First steps to D3a
    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+//------------Button----------------//
+const messageButton = document.createElement("button");
+messageButton.textContent = "Click me!";
+messageButton.classList.add("message-button");
+
+// Append the new button to the app div
+app.appendChild(messageButton);
+
+// Add a click event listener to the new button
+messageButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
